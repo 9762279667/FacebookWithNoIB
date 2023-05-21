fab2210643edbf74bfcaafad008b1a4fc4ff833e
Nitin Kalokhe <nitin.kalokhe@godigit.com>
2023-05-21 20:54:07 +0530

    Initial Commit

---
+++ FacebookWithNoIB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FacebookWithNoIB
+//
+//  Created by Nitin Kalokhe on 21/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
